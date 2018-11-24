a3efc76f464899a802b37d415dc98f8804c3144e
Florian Adonis <tanohzana@Florians-MacBook-Pro.local>
2018-11-24 13:04:31 +0100

    fix: modifies how precommit is done

--- dist/src/utils/extractPackagesToInstall.js
+++ dist/src/utils/extractPackagesToInstall.js
@@ -6,14 +6,17 @@ Object.defineProperty(exports, "__esModule", {
 // Finds all packages to install in a file
 var extractPackagesToInstall = function extractPackagesToInstall(fileContent) {
   var packages = fileContent.match(/require\(["'][\x2D0-9A-Z_a-z\u017F\u212A]+["']\)/gi);
+
+  if (!packages) {
+    // eslint-disable-next-line
+    console.log('No packages to install');
+
+    return [];
+  }
+
   var mappedPackages = packages.map(function (pack) {
     return pack.substr(9, pack.length - 11);
   });
-
-  if (!mappedPackages) {
-    // eslint-disable-next-line
-    console.log('No packages to install');
-  }
 
   return mappedPackages;
 };
